31f23b07a4b9dceaefef3f82b1af43352cc35cd7
shoeb-gg <1920038@iub.edu.bd>
2021-11-13 15:01:03 +0600

    Connected to fake store API, fetching data and outputting it to frontend

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,34 @@
-import React, {useState} from 'react'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import tee from '../resources/tee.jpg'
-import style from "../resources/card.css"
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import style from "../resources/card.css";
 
-export default function CardProduct() {
-    const [count, setCount] = useState("Add to Cart");
-    const handleClick = () =>{
-        if(count==="Add to Cart"){
-            setCount(parseInt(1))
-            return
-        }    
-        setCount(count+1)
+const CardProduct = ({ desc, image }) => {
+  const [count, setCount] = useState("Add to Cart");
+  const handleClick = () => {
+    if (count === "Add to Cart") {
+      setCount(parseInt(1));
+      return;
     }
-    return (
-        <div className="c">
-            <Card className="text-center" className="cStyle">
-                <Card.Img variant="top" src={tee} className="cImg"/>
-                <Card.Body>
-                    <Card.Text>
-                        Pruduct Desc
-                    </Card.Text>                    
-                </Card.Body>
-                <Button onClick={handleClick} variant="primary" size="sm" style={{flex: "auto"}}>{count}</Button>
-            </Card>
-            
-        </div>
-    )
-}
+    setCount(count + 1);
+  };
+  return (
+    <div className="c">
+      <Card className="text-center" className="cStyle">
+        <Card.Img variant="top" src={image} className="cImg" />
+        <Card.Body>
+          <Card.Text className="des">{desc}</Card.Text>
+        </Card.Body>
+        <Button
+          onClick={handleClick}
+          variant="primary"
+          size="sm"
+          style={{ flex: "auto" }}
+        >
+          {count}
+        </Button>
+      </Card>
+    </div>
+  );
+};
+export default CardProduct;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import CardProduct from './Card'
+import React, { useEffect, useState } from "react";
+import CardProduct from "./Card";
+import style from "../resources/products.css";
 
+export default function Products() {
+  const [prod, setProd] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
 
-export default function Products() { 
-    
-    return (
-        <div>
-           <CardProduct/>
-        </div>
-    )
+    const data = await response.json();
+    console.log(data);
+    setProd(data);
+  };
+
+  return (
+    <div className="produ">
+      {prod.map((prods) => (
+        <CardProduct
+          key={prods.title}
+          desc={prods.description}
+          image={prods.image}
+        />
+      ))}
+    </div>
+  );
 }
